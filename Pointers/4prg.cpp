ee61d559ef37ee0c9874200b7c8528677871bca4
YashKhati <103344945+YashKhati@users.noreply.github.com>
2022-08-06 20:42:20 +0530

    Create 4prg.cpp
    pointer in function 
    Line  will give error inside int getSum(int arr[],int n)  function :
        cout<<"Size of arr "<<sizeof(arr);
        error : main.cpp: In function ‘int getSum(int*, int)’:
    main.cpp:19:52: warning: ‘sizeof’ on array function parameter ‘arr’ will return size of ‘int*’ [-Wsizeof-array-argument]
          cout<<"Size of arr inside function "<<sizeof(arr)<<endl;
                                                                                 ^ 
            main.cpp:17:16: note: declared here
            int getSum(int arr[],int n)

---
+++ Pointers/4prg.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include<array>
+using namespace std;
+void update(int* ptr)
+{
+   cout<<"Inside function Address of ptr = "<<&ptr<<endl;
+   cout<<"Inside function ptr = "<<ptr<<endl ;                  // ptr inside update is not same as ptr in main though they contain  same address 
+   ptr = ptr + 1;
+   cout<<"Inside update function  ptr = "<<ptr<<endl;
+}
+
+void updateVal(int *ptr)
+{
+   *ptr=*ptr + 1;
+}
+
+int getSum(int arr[],int n)
+{  
+   cout<<"Size of arr inside function "<<sizeof(arr)<<endl; 
+   int sum=0;
+   for(int i=0;i<n;i++)
+   {
+     sum+=arr[i];
+   }
+   return sum;
+}
+int main()
+{
+   int num=5;
+
+	int *ptr=&num;
+   
+   cout<<"Address of ptr = "<<&ptr<<endl;
+   cout<<"Before ptr =  "<<ptr<<endl;
+   update(ptr);
+   cout<<"After ptr =  "<<ptr<<endl;
+
+   cout<<"\n\n";
+   cout<<"Before update *ptr =  "<<*ptr<<endl;
+   updateVal(ptr);   //update value in address stored in ptr by 1
+   cout<<"After update *ptr =  "<<*ptr<<endl;
+     
+   cout<<"\n\n";
+   int arr[5]={1,2,3,4,5};
+   cout<<"Array size : "<<sizeof(arr)<<endl;
+   cout<<"Sum of Array : "<<getSum(arr,5)<<endl;
+}
